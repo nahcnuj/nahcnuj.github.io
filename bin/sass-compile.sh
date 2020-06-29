d11c9da91884f992f319f20e9b3aa6cea148f3aa
Junichi Hayashi <junchan.kcct@gmail.com>
2020-06-29 22:52:07 +0900

    Compress CSS files

--- bin/sass-compile.sh
+++ bin/sass-compile.sh
@@ -3,4 +3,4 @@ mkdir -p css && \
 find -name '*.scss' \
     | grep -v '.*/_.*\.scss$' \
     | sed -e 's,^\./sass/\(.*\)\.scss,sass/\1.scss css/\1.css,' \
-    | xargs -n2 sassc
+    | xargs -n2 sassc -t compressed
