3b8e4ba8e6a4c2a6dfd9cefd225841a05d679921
Junichi Hayashi <junchan.kcct@gmail.com>
2020-07-23 12:25:12 +0900

    Change owner of files built by rmd2mustache from root to host user

--- bin/run-raku-script.sh
+++ bin/run-raku-script.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
-zef install --deps-only .
-raku "$@"
+
+if [ ! -z ${LOCAL_UID} -o ! -z ${LOCAL_GID} ]; then
+    USER_ID=${LOCAL_UID:-1000}
+    GROUP_ID=${LOCAL_GID:-1000}
+
+    addgroup -S -g $GROUP_ID user
+    adduser -S -u $USER_ID -G user user
+    export HOME=/home/user
+    SU='su-exec user'
+fi
+
+${SU} raku "$@"
