fb96787bc008868925626d2bd139be1b9dda6cf4
Junichi Hayashi <junchan.kcct@gmail.com>
2022-01-23 08:23:14 +0900

    change uid,gid

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -24,7 +24,7 @@ fi
 
 # change owner of directories under HOME to builder
 find . -type d -perm u=w \
-    | xargs -r chown -f -R ${uid}:${gid} \
+    | xargs -r chown -f -R ${USER}:${USER} \
     || true
 ls -alR
 
