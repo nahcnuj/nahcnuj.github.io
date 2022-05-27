7647498c0cba2bab0e974fbf3c376fecd98f16e0
nahcnuj <nahcnuj@users.noreply.github.com>
2022-05-27 15:58:56 +0000

    deploy: 253369d77a54454f2c9acb01c378ebae3e8cecd5

--- nuxt-multibyte-url-demo/_nuxt/static/1653664795/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-multibyte-url-demo\u002F_nuxt\u002Fstatic\u002F1653664795",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-multibyte-url-demo\u002F",assetsPath:"\u002Fnuxt-multibyte-url-demo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
