b0ff0c18634254afde441cc295acd8aba012df3e
nahcnuj <nahcnuj@users.noreply.github.com>
2022-05-28 03:48:23 +0000

    deploy: af4f23bb5b98d181d8db60a609dff61c05e3042b

---
+++ nuxt-multibyte-url-fixed/_nuxt/static/1653709568/hoge/日本語/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-multibyte-url-fixed\u002F_nuxt\u002Fstatic\u002F1653709568",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhoge\u002F%E6%97%A5%E6%9C%AC%E8%AA%9E",config:{_app:{basePath:"\u002Fnuxt-multibyte-url-fixed\u002F",assetsPath:"\u002Fnuxt-multibyte-url-fixed\u002F_nuxt\u002F",cdnURL:a}}}}(null));
