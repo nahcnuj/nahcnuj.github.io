a13ba5d7298d353270886903d1e5939d1aa6e970
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-03-01 14:17:32 +0000

    add index content

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,18 +1,32 @@
-import { Style } from 'hono/css'
+import { Style, css } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
 import { Script } from 'honox/server'
 
-export default jsxRenderer(({ children, title }) => {
+export default jsxRenderer(({ children, title, description }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title}</title>
+        <title>{title ?? ''}</title>
+        {description && <meta name="description" content={description} />}
+        {title && <meta property="og:title" content={title} />}
+        {description && <meta property="og:description" content={description} />}
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://img.nahcnuj.work/author.jpg" />
+        <meta property="og:image:alt" content="Junichi's face" />
         <Script src="/app/client.ts" async />
-        <Style />
+        <Style>{css`
+          html, body { margin: 0; padding: 0; }
+          body > * { margin-inline: auto; max-width: 40em; font-size: 1.2em; }
+        `}</Style>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <footer>
+          <p>&copy; Junichi Hayashi</p>
+        </footer>
+      </body>
     </html>
   )
 })
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,52 @@
-import { css } from 'hono/css'
 import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
-
-const className = css`
-  font-family: sans-serif;
-`
 
 export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'Hono'
   return c.render(
-    <div class={className}>
-      <h1>Hello, {name}!</h1>
-      <Counter />
-    </div>,
-    { title: name },
+    <main>
+      <header style="text-align:center">
+        <h1>Junichi Hayashi, a web engineer</h1>
+        <div>
+          <img src="https://img.nahcnuj.work/author.jpg" width="200" height="200" alt="Junichi's face" />
+        </div>
+      </header>
+
+      <h2>I am a ...</h2>
+      <p>都内の某企業で会社員として働いているウェブエンジニアです。</p>
+
+      <h2>I like ...</h2>
+      <h3>プログラミング</h3>
+      <p>
+        小学5年生のときに父が所持していたポケットコンピュータでBASICと出会ってから、
+        <abbr title="Hot Soup Processor">HSP</abbr>、C、C++、C#、Java、Perl、TypeScript、Go、Rust、Coq、Lean 4など、
+        様々な言語でプログラミングを楽しんでいます。
+      </p>
+
+      <h3>Perl</h3>
+      <p>仕事で主に使っているプログラミング言語です。なんとなく「手作り感」があるのが好きです。</p>
+
+      <h3>TypeScript</h3>
+      <p>
+        Next.jsやNuxtを使ったウェブサイト構築に挑戦するのと同時に手を出した言語です。
+        それまで扱っていた動的型付けなPerlと対比して、静的に型が付くことの便利さを再確認させてくれました。
+      </p>
+
+      <h3>型</h3>
+      <p>プログラムの型注釈と命題の証明がカリー＝ハワード同型対応によって結び付くという強力さに惚れました。</p>
+
+      <h3>東山奈央さん（声優・歌手）</h3>
+      <p>
+        2ndシングル「イマココ/月がきれい」を聴いて、そのCDのリリースイベントに足を運んだところから歌手として好きになりました。
+        ソロライブには1stライブRainbowからすべてのライブに行っています（流石に全公演ではありません…）。
+      </p>
+
+      <h2>I was ...</h2>
+      <ul>
+        <li>神戸市立工業高等専門学校電気工学科 卒業</li>
+        <li>大阪大学基礎工学部情報科学科 卒業</li>
+        <li>大阪大学大学院情報科学研究科コンピュータサイエンス専攻 中退</li>
+        <li>シーサー株式会社 正社員</li>
+      </ul>
+    </main>,
+    { title: 'Junichi Hayashi, a web engineer' },
   )
 })
