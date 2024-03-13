70bf3696663a399a54525d97845370978875bf79
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-03-13 20:14:49 +0900

    wip

--- app/routes/works/_renderer.tsx
+++ app/routes/works/_renderer.tsx
@@ -1,7 +1,7 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 
-export default jsxRenderer(({ children, Layout, frontmatter }) => (
-  <Layout {...frontmatter}>
+export default jsxRenderer(({ children, Layout, frontmatter, title, description }) => (
+  <Layout {...frontmatter} title={title} description={description}>
     <article>{children}</article>
   </Layout>
 ))
