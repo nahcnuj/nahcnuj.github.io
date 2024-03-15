c7e9fb945285162ffc9729f5cd9571abfbf613e3
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-03-15 19:16:41 +0900

    style header nav

--- app/components/RootHeader.tsx
+++ app/components/RootHeader.tsx
@@ -1,16 +1,44 @@
+import { css } from 'hono/css'
+
+type NavItem = {
+  title: string
+  href: `/${string}`
+}
+
+const navItems = [
+  { title: 'Index', href: '/' },
+  { title: 'Works', href: '/works/index.html' },
+] satisfies NavItem[]
+
 export default function RootHeader() {
   return (
     <header>
       <nav>
-        <ul>
-          <li>
-            <a href="/">Index</a>
-          </li>
-          <li>
-            <a href="/works/">Works</a>
-          </li>
-        </ul>
+        <NavList items={navItems} />
       </nav>
     </header>
+  )
+}
+
+const listClass = css`
+  display: grid;
+  grid-template-columns: repeat(${Math.max(navItems.length, 6)}, 1fr);
+  list-style: none;
+  font-size: smaller;
+`
+
+const itemClass = css`
+  display: inline-block;
+`
+
+function NavList({ items }: { items: NavItem[] }) {
+  return (
+    <ul class={listClass}>
+      {items.map(({ href, title }) => (
+        <li class={itemClass}>
+          <a href={href}>{title}</a>
+        </li>
+      ))}
+    </ul>
   )
 }
