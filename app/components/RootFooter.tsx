008896a53dcdb3705335a2fbf212526a599086a6
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-03-16 19:48:28 +0900

    wip

--- app/components/RootFooter.tsx
+++ app/components/RootFooter.tsx
@@ -1,7 +1,12 @@
+import { css } from 'hono/css'
+
+const footerClass = css`
+  margin-top: 2em;
+  padding-top: 1em;
+  border-top: thin solid #333;
+  font-size: smaller;
+`
+
 export default function RootFooter() {
-  return (
-    <footer>
-      <p>&copy; Junichi Hayashi</p>
-    </footer>
-  )
+  return <footer class={footerClass}>&copy; Junichi Hayashi</footer>
 }
