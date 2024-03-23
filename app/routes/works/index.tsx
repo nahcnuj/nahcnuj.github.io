a34266dc71952685ffa5bd091db7386b532b7a5a
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-03-23 23:54:56 +0900

    enable trailingSlash option

--- app/routes/works/index.tsx
+++ app/routes/works/index.tsx
@@ -1,33 +1,26 @@
-import { Hono } from 'hono'
+import { createRoute } from 'honox/factory'
 import WorkList from '../../components/WorkList'
 import type { Meta } from './type'
 
-const app = new Hono()
+export default createRoute((c) => {
+  const title = `Junichi Hayashi's Works`
+  const description = 'There are the works Junichi Hayashi has made.'
 
-app.get(
-  '/index', // for compatibility, generates as /works/index.html
-  (c) => {
-    const title = `Junichi Hayashi's Works`
-    const description = 'There are the works Junichi Hayashi has made.'
+  const works = import.meta.glob<{ frontmatter: Meta }>('./**/*.mdx', {
+    eager: true,
+  })
 
-    const works = import.meta.glob<{ frontmatter: Meta }>('./**/*.mdx', {
-      eager: true,
-    })
+  return c.render(
+    <>
+      <h1>Works</h1>
+      <p>私が制作したモノの一覧です。</p>
 
-    return c.render(
-      <>
-        <h1>Works</h1>
-        <p>私が制作したモノの一覧です。</p>
-
-        <WorkList
-          works={Object.entries(works)
-            .sort(([a], [b]) => b.localeCompare(a))
-            .map(([filename, { frontmatter }]) => [filename.replace(/\.mdx$/, '.html'), frontmatter])}
-        />
-      </>,
-      { title, description },
-    )
-  },
-)
-
-export default app
+      <WorkList
+        works={Object.entries(works)
+          .sort(([a], [b]) => b.localeCompare(a))
+          .map(([filename, { frontmatter }]) => [filename.replace(/\.mdx$/, '.html'), frontmatter])}
+      />
+    </>,
+    { title, description },
+  )
+})
