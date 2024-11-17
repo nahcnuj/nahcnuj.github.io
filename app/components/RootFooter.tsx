d20384b8dff6a6a3e7c60f496a13116e5a8c870e
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2024-11-17 04:43:36 +0000

    add some links in the footer

--- app/components/RootFooter.tsx
+++ app/components/RootFooter.tsx
@@ -5,8 +5,19 @@ const footerClass = css`
   padding-block: 1em;
   border-top: 1pt solid var(--theme-main-color);
   font-size: smaller;
+
+  display: flex;
+  aligns-item: center;
+  justify-content: space-between;
 `
 
 export default function RootFooter() {
-  return <footer class={footerClass}>&copy; Junichi Hayashi</footer>
+  return <footer class={footerClass}>
+    <div>&copy; Junichi Hayashi</div>
+    <div>
+      <a href="//x.com/pronahcnuj" rel="me">X</a>
+      {' | '}
+      <a href="//github.com/nahcnuj" rel="me">GitHub</a>
+    </div>
+  </footer>
 }
