644a1f940e600fae4402fa7c0302bad61dd75d5a
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-02-21 17:27:02 +0000

    create Layout component

---
+++ app/components/RootLayout.tsx
@@ -0,0 +1,87 @@
+import { Style, css } from 'hono/css'
+import { html } from 'hono/html'
+import type { PropsWithChildren } from 'hono/jsx'
+import { Script } from 'honox/server'
+
+type Meta = {
+  title: string
+  description?: string
+  ogImage?: string
+  ogImageAlt?: string
+  headInjection?: unknown
+}
+
+const rootStyle = css`
+  html, body { margin: 0; padding: 0; }
+
+  :root {
+    --theme-base-color: #e6e6ff;
+    --theme-main-color: #000033;
+    --theme-accent-color: #ff6666;
+  }
+
+  html {
+    background: var(--theme-base-color);
+    color: var(--theme-main-color);
+
+    scrollbar-gutter: stable both-edges;
+  }
+
+  a:visited {
+    color: var(--theme-main-color);
+  }
+  a:active, a:hover {
+    color: var(--theme-accent-color);
+  }
+`
+
+const Layout = (props: PropsWithChildren<Meta>) => html`
+<html>
+<head prefix="og: http://ogp.me/ns#">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${props.title}</title>
+  ${props.description ? html`<meta name="description" content="${props.description}">` : ''}
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="${props.title}">
+  ${props.description ? html`<meta property="og:description" content="${props.description}">` : ''}
+  ${props.ogImage ? html`<meta property="og:image" content="${props.ogImage}">` : ''}
+  ${props.ogImageAlt ? html`<meta property="og:image:alt" content="${props.ogImageAlt}">` : ''}
+  ${<Script src="/app/client.ts" async />}
+  ${<Style>{rootStyle}</Style>}
+  <!-- Gtag -->
+  <link rel="preload" href="https://www.googletagmanager.com/gtag/js?id=G-RMH8Q8RB96" as="script">
+  <!-- / Gtag -->
+  <!-- ninja access -->
+  <!-- <link rel="preload" href="https://x4.shinobi.jp/ufo/060401300" as="script"> -->
+  <!-- / ninja access -->
+  ${props.headInjection}
+</head>
+<body>
+  ${props.children}
+  <!-- Gtag -->
+  <script src="https://www.googletagmanager.com/gtag/js?id=G-RMH8Q8RB96" async></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-RMH8Q8RB96');
+  </script>
+  <!-- /Gtag -->
+  <!-- ninja access -->
+  <!-- <script type="text/javascript" src="//x4.shinobi.jp/ufo/060401300"></script> -->
+  <noscript>
+    <a href="//x4.shinobi.jp/bin/gg?060401300" target="_blank" rel="noreferrer">
+      <img src="//x4.shinobi.jp/bin/ll?060401300" border="0" alt="">
+    </a>
+    <br>
+    <span style="font-size:9px">
+      <img style="margin:0;vertical-align:text-bottom;" src="//img.shinobi.jp/tadaima/fj.gif" width="19" height="11" alt="">
+    </span>
+  </noscript>
+  <!-- / ninja access -->
+</body>
+</html>
+`
+
+export default Layout
