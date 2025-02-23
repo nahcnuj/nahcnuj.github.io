5347771ec8e26206dee085097df29012961215b8
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-02-23 07:56:03 +0000

    modify advertisement design

--- app/components/AdMax.tsx
+++ app/components/AdMax.tsx
@@ -10,11 +10,37 @@ interface Props {
 }
 
 const admaxClass = css`
-  height: 100%;
-  display: grid;
-  justify-items: center;
-  align-content: space-around;
-  text-align: center;
+  position: relative;
+  height: calc(100% + 1.2rem);
+  box-sizing: border-box;
+  padding-block-end: 0.5rem;
+
+  background: linear-gradient(to bottom,
+    rgba(173, 216, 230, 0.5),
+    var(--theme-base-color) 5%,
+    var(--theme-base-color) 95%,
+    rgba(173, 216, 230, 0.5)
+  );
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+
+  &::before {
+    position: absolute;
+    top: -0.8rem;
+    left: 0.8rem;
+    padding: 0.05rem 0.4rem;
+
+    background-color: color-mix(in srgb, var(--theme-base-color) 50%, lightblue);
+    border-radius: 0.1rem;
+    box-shadow: 0 0.1rem 0.1rem rgba(0, 0, 0, 0.1);
+
+    content: "Advertisement";
+    font-family: serif;
+    font-size: 0.8rem;
+  }
 `
 
 const fullWidthClass = css`
@@ -29,7 +55,7 @@ const fullWidthClass = css`
  */
 export default function AdMax({ height, fullWidth, children }: PropsWithChildren<Props>) {
   return (
-    <aside class={css`height:${height}`}>
+    <aside class={css`margin-block-start:1.5rem;margin-block-end:2rem;height:${height}`}>
       <div class={cx(admaxClass, fullWidth && fullWidthClass)}>{children}</div>
     </aside>
   )
