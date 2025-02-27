4a86967e88e39f97bfd9c4848acd10c3d98ace23
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2025-02-27 21:58:17 +0900

    fix the page header to the top

--- app/components/RootHeader.tsx
+++ app/components/RootHeader.tsx
@@ -7,33 +7,53 @@ const navItems = [
   { title: 'Essay', href: '/essays/index.html' as const },
 ]
 
+const headerClass = css`
+  width: 100%;
+
+  position: sticky;
+  top: 0;
+  z-index: 1;
+
+  display: flex;
+  justify-content: space-evenly;
+
+  background: var(--theme-base-color);
+  box-shadow: var(--theme-base-color) 0 2px 5px 5px;
+`
+
 const listClass = css`
   --columns: 4;
   // @media screen and (min-width: 600px) {
   //   --columns: 6;
   // }
 
+  margin-block: 0.5em;
   padding-inline-start: 0;
 
   display: grid;
   grid-template-columns: repeat(var(--columns), 1fr);
-  grid-row-gap: 1rem;
 
   list-style: none;
-  font-size: smaller;
+  // font-size: smaller;
   text-align: center;
 `
 
 const itemClass = css`
   display: inline-block;
-  line-height: 2;
+  height: 2em;
+  line-height: 2em;
+
+  & a {
+    display: inline-block;
+    width: 80%;
+  }
 `
 
 export default function RootHeader() {
   return (
     <>
-      <header>
-        <nav>
+      <header class={headerClass}>
+        <nav class={css`width:100%`}>
           <ul class={listClass}>
             {navItems.map(({ href, title }) => (
               <li key={href} class={itemClass}>
